0cce9cb356648ca9d568758ebe38a34b40a8dd8f
KarolineOberek <karolineoberek@gmail.com>
2024-06-17 10:25:04 -0300

    função de sim ou não

--- instrumentos_github.js
+++ instrumentos_github.js
@@ -5,6 +5,16 @@ let resposta;
 process.stdin.on("data", function (data){
     let entrada = data.toString().trim();
     if (!resposta){
-        resposta
+        resposta = entrada;
+        if (resposta == "sim" || resposta == "s"){
+            console.log("Que ótimo")
+        }else if (resposta == "n" || resposta == "nao" || resposta == "não"){
+            console.log("Que pena!")
+            process.exit()
+        }
+    }
+    else{
+        console.log("Escreva uma resposta válida: sim(s) ou não(n)")
+        resposta = undefined;
     }
 })
